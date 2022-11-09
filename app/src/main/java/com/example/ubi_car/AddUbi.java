6d564a87d7833bd67de909193fbcb7af512ea9a1
RjacoboR <jacoboman55@gmail.com>
2022-11-09 16:40:27 +0100

    0.6 Subida
    Ya se pueden realizar subidas de datos desde la app a la API

--- app/src/main/java/com/example/ubi_car/AddUbi.java
+++ app/src/main/java/com/example/ubi_car/AddUbi.java
@@ -2,18 +2,44 @@ package com.example.ubi_car;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import java.io.UnsupportedEncodingException;
+
 public class AddUbi extends AppCompatActivity {
+
+    private EditText edit1,edit2,edit3,edit4;
+    String coche;
+    String referencia;
+    String matricula;
+    String dia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ubi);
+        //Asigno valores a los EditText
+        edit1 =(EditText) findViewById(R.id.editTextCoche);
+        edit2 =(EditText) findViewById(R.id.editTextTextReferencia);
+        edit3 =(EditText) findViewById(R.id.editTextTextMatricula);
+        edit4 =(EditText) findViewById(R.id.editTextTextDia);
     }
 
     public void onClickVolv (View view){
@@ -27,5 +53,36 @@ public class AddUbi extends AppCompatActivity {
         CharSequence text ="Funciona";
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    public void onClickGuardarFinal (View view) throws UnsupportedEncodingException{
+        coche=edit1.getText().toString();
+        referencia=edit2.getText().toString();
+        matricula=edit3.getText().toString();
+        dia=edit4.getText().toString();//?
+        Mirequest post = new Mirequest(Request.Method.POST,"https://63564ad8a2d1844a9771151f.mockapi.io/api/uc/Coche",new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response){
+                System.out.print("Guardado");
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                CharSequence text ="Enviado";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                System.out.print("No se ha guardado");
+                Context context=getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                CharSequence text ="Fallo al guardar";
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        },coche,referencia,matricula,dia);
+        RequestQueue cola= Volley.newRequestQueue(this);
+        cola.add(post);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/ubi_car/Mirequest.java
@@ -0,0 +1,37 @@
+package com.example.ubi_car;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
+
+public class Mirequest extends StringRequest {
+
+    String coche,referencia,matricula,dia;
+
+    String dato1,dato2,dato3,dato4;
+
+    public Mirequest(int method, String url, Response.Listener<String> listener, @Nullable Response.ErrorListener errorListener,
+                     String coche, String referencia, String matricula, String dia){
+        super(method, url, listener, errorListener);
+        this.coche=coche;
+        this.referencia=referencia;
+        this.matricula=matricula;
+        this.dia=dia;
+    }
+
+    //@Override
+    public Map<String,String> hetHeaders()throws AuthFailureError{
+        return super.getHeaders();
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError{
+        String chichaPeticion=String.format("coche=%s$referencia=%s$matricula=%s$dia",coche,referencia,matricula,dia);
+        byte[] b = chichaPeticion.getBytes();
+        return b;
+    }
+}
